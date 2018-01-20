dd4e35a6cd5e6bef80e388b4aac033255dfad1b3
Patrick Buhagiar <patrickbhgr@gmail.com>
2018-01-20 23:07:16 +0000

    Added UI for Advert Admin Console

--- src/views/home/Home.react.tsx
+++ src/views/home/Home.react.tsx
@@ -1,19 +1,238 @@
 import React from 'react'
-import { Image } from 'react-bootstrap'
+import { Button, Card, CardBody, CardImage, CardTitle, CardText } from 'mdbreact';
 
 export class Home extends React.Component<any, any> {
     render() {
         return (
-            <div style={{ textAlign: "center", backgroundColor: "#181818" }}>
-                <div>
-                    <Image src="http://greatunihack.com/images/guh_logo.png" href="http://greatunihack.com/" alt="500x500" style={{ width: "500px" }} circle />
+            <div>
+                <h1>Advertiser Portal</h1>
+
+                <div className="row flex-row flex-nowrap">
+                    <div className="col-3">
+                        <div className="card">
+                            <img className="card-img-top" src="http://www.guy-sports.com/fun_pictures/colgate.jpg" alt="Card image cap" />
+                            <div className="card-block">
+                                <h2 className="card-title">Colgate Advert</h2>
+                                <div>
+                                    <label>Emotion</label>
+                                    <select>
+                                        <option value="none">None</option>
+                                        <option value="happy">Happy</option>
+                                        <option value="sad">Sad</option>
+                                        <option value="angry">Angry</option>
+                                        <option value="nuetral">Neutral</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label>Age Group</label>
+                                    <select>
+                                        <option value="all">All</option>
+                                        <option value="0-12">Children</option>
+                                        <option value="13-19">Teenager</option>
+                                        <option value="20-30">Young Adult</option>
+                                        <option value="31-60">Adult</option>
+                                        <option value="60+">Old</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label>Gender</label>
+                                    <select>
+                                        <option value="none">Both</option>
+                                        <option value="happy">Male</option>
+                                        <option value="nuetral">Female</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="card">
+                            <img className="card-img-top" src="https://i.pinimg.com/736x/60/6d/c0/606dc0d353991cd35950cd4059c45199--creative-posters-funny-ads.jpg" alt="Card image cap" />
+                            <div className="card-block">
+                                <h2 className="card-title">Durex Advert</h2>
+                                <div>
+                                    <label>Emotion</label>
+                                    <select>
+                                        <option value="none">None</option>
+                                        <option value="happy">Happy</option>
+                                        <option value="sad">Sad</option>
+                                        <option value="angry">Angry</option>
+                                        <option value="nuetral">Neutral</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label>Age Group</label>
+                                    <select>
+                                        <option value="all">All</option>
+                                        <option value="0-12">Children</option>
+                                        <option value="13-19">Teenager</option>
+                                        <option value="20-30">Young Adult</option>
+                                        <option value="31-60">Adult</option>
+                                        <option value="60+">Old</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label>Gender</label>
+                                    <select>
+                                        <option value="none">Both</option>
+                                        <option value="happy">Male</option>
+                                        <option value="nuetral">Female</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="card">
+                            <img className="card-img-top" src="http://wwwcdn.howdesign.com/wp-content/uploads/funny-ad-chicken.jpg" alt="Card image cap" />
+                            <div className="card-block">
+                                <h2 className="card-title">Orbit Advert</h2>
+                                <div>
+                                    <label>Emotion</label>
+                                    <select>
+                                        <option value="none">None</option>
+                                        <option value="happy">Happy</option>
+                                        <option value="sad">Sad</option>
+                                        <option value="angry">Angry</option>
+                                        <option value="nuetral">Neutral</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label>Age Group</label>
+                                    <select>
+                                        <option value="all">All</option>
+                                        <option value="0-12">Children</option>
+                                        <option value="13-19">Teenager</option>
+                                        <option value="20-30">Young Adult</option>
+                                        <option value="31-60">Adult</option>
+                                        <option value="60+">Old</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label>Gender</label>
+                                    <select>
+                                        <option value="none">Both</option>
+                                        <option value="happy">Male</option>
+                                        <option value="nuetral">Female</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="card">
+                            <img className="card-img-top" src="https://s-media-cache-ak0.pinimg.com/originals/d6/71/d8/d671d8bc50cbbc2686388a1bc15b715a.jpg" alt="Card image cap" />
+                            <div className="card-block">
+                                <h2 className="card-title">H&M Advert </h2>
+                                <div>
+                                    <label>Emotion</label>
+                                    <select>
+                                        <option value="none">None</option>
+                                        <option value="happy">Happy</option>
+                                        <option value="sad">Sad</option>
+                                        <option value="angry">Angry</option>
+                                        <option value="nuetral">Neutral</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label>Age Group</label>
+                                    <select>
+                                        <option value="all">All</option>
+                                        <option value="0-12">Children</option>
+                                        <option value="13-19">Teenager</option>
+                                        <option value="20-30">Young Adult</option>
+                                        <option value="31-60">Adult</option>
+                                        <option value="60+">Old</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label>Gender</label>
+                                    <select>
+                                        <option value="none">Both</option>
+                                        <option value="happy">Male</option>
+                                        <option value="nuetral">Female</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="card">
+                            <img className="card-img-top" src="https://2.bp.blogspot.com/-6CRbAr-DulY/VEDZM2w4IYI/AAAAAAAAWMo/h2MBS4Tbwlo/s1600/funny-dog-weightloss-advert.jpg" alt="Card image cap" />
+                            <div className="card-block">
+                                <h2 className="card-title">Dog Food Advert</h2>
+                                <div>
+                                    <label>Emotion</label>
+                                    <select>
+                                        <option value="none">None</option>
+                                        <option value="happy">Happy</option>
+                                        <option value="sad">Sad</option>
+                                        <option value="angry">Angry</option>
+                                        <option value="nuetral">Neutral</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label>Age Group</label>
+                                    <select>
+                                        <option value="all">All</option>
+                                        <option value="0-12">Children</option>
+                                        <option value="13-19">Teenager</option>
+                                        <option value="20-30">Young Adult</option>
+                                        <option value="31-60">Adult</option>
+                                        <option value="60+">Old</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label>Gender</label>
+                                    <select>
+                                        <option value="none">Both</option>
+                                        <option value="happy">Male</option>
+                                        <option value="nuetral">Female</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="card">
+                            <img className="card-img-top" src="https://i.pinimg.com/736x/1b/ed/a9/1beda91785bc9c2a77983271cbca062a--old-spice-funny-ads.jpg" alt="Card image cap" />
+                            <div className="card-block">
+                                <h2 className="card-title">Old Spice advert</h2>
+                                <div>
+                                    <label>Emotion</label>
+                                    <select>
+                                        <option value="none">None</option>
+                                        <option value="happy">Happy</option>
+                                        <option value="sad">Sad</option>
+                                        <option value="angry">Angry</option>
+                                        <option value="nuetral">Neutral</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label>Age Group</label>
+                                    <select>
+                                        <option value="all">All</option>
+                                        <option value="0-12">Children</option>
+                                        <option value="13-19">Teenager</option>
+                                        <option value="20-30">Young Adult</option>
+                                        <option value="31-60">Adult</option>
+                                        <option value="60+">Old</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label>Gender</label>
+                                    <select>
+                                        <option value="none">Both</option>
+                                        <option value="happy">Male</option>
+                                        <option value="nuetral">Female</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <Image src="https://pre00.deviantart.net/47d2/th/pre/i/2017/115/a/6/rick_and_morty_png_by_lalingla-db72d4x.png" />
-                </div>
-                <div>
-                    <Image src="https://res.cloudinary.com/teepublic/image/private/s--79RqElHn--/t_Preview/b_rgb:191919,c_limit,f_auto,h_313,q_90,w_313/v1496645914/production/designs/1646551_1" />
-                </div>
+                <br/>
+                <button className="btn btn-success">Save</button>
             </div>
         )
     }
